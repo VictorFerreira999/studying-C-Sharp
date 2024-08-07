8450d7c0b7b32f12a58de47c65be3b5bf491580a
VictorFerreira999 <victorferreira9990@gmail.com>
2024-08-07 09:02:00 -0300

    Divisao

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,7 +7,7 @@ namespace Calculator
       static void Main(string[] args)
       {
  
-         Soma();
+         Divisao();
 
       }
 
@@ -38,10 +38,29 @@ namespace Calculator
          Console.WriteLine("Segundo valor: ");
          float v2 = float.Parse(Console.ReadLine());
 
+         Console.WriteLine("");
+         
          float resultado = v1 - v2;
          Console.WriteLine($"O resultado da subtração é: {resultado}");
          Console.ReadKey();
 
+      }
+   
+      static void Divisao()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor: ");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor: ");
+         float v2 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("");
+
+         float resultado = v1 / v2;
+         Console.WriteLine($"O resultado da divisão é {resultado}");
+         Console.ReadKey();
       }
    }
 }
