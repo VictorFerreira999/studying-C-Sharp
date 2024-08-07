c4c44bcb61b20d2321602ac84fc31ddd590ac77e
VictorFerreira999 <victorferreira9990@gmail.com>
2024-08-07 08:47:16 -0300

    subtração

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,6 +6,13 @@ namespace Calculator
    {
       static void Main(string[] args)
       {
+ 
+         Soma();
+
+      }
+
+      static void Soma()
+      {
          Console.Clear();
 
          Console.WriteLine("Primeiro valor: ");
@@ -17,10 +24,24 @@ namespace Calculator
          Console.WriteLine("");
 
          float resultado = v1 + v2;
-         Console.WriteLine("O resultado da soma é:" + resultado);
          Console.WriteLine($"O resultado da soma é: {resultado}");
+         Console.ReadKey();
+      }
 
-         
+      static void Subtracao()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor: ");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor: ");
+         float v2 = float.Parse(Console.ReadLine());
+
+         float resultado = v1 - v2;
+         Console.WriteLine($"O resultado da subtração é: {resultado}");
+         Console.ReadKey();
+
       }
    }
 }
