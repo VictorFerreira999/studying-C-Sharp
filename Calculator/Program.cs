9f5b057bd1c39dce76a9a058a27c613bbd6c3b3f
VictorFerreira999 <victorferreira9990@gmail.com>
2024-08-08 08:51:09 -0300

    sair do programa

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -20,6 +20,7 @@ namespace Calculator
          Console.WriteLine("2 -Subtração");
          Console.WriteLine("3 - MUltiplicação");
          Console.WriteLine("4 - Divisão");
+         Console.WriteLine("Digite 5 para sair do programa");
 
          Console.WriteLine("--------------------");
          Console.WriteLine("Selecione uma opção: ");
@@ -31,6 +32,7 @@ namespace Calculator
             case 2: Subtracao(); break;
             case 3: Multiplicacao(); break;
             case 4: Divisao(); break;
+            case 5: System.Environment.Exit(0); break;
             default: Menu(); break;
          }
       }
